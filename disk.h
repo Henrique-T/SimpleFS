62ba446b66e6ed48cb4ccb4be878bda63ca984c3
Henrique Tridapalli <henrique.tridapalli@getmore.com>
2023-11-17 20:02:13 -0300

    Add general comments

--- disk.h
+++ disk.h
@@ -10,25 +10,28 @@ using namespace std;
 class Disk
 {
 public:
-    static const unsigned short int DISK_BLOCK_SIZE = 4096;
-    static const unsigned int DISK_MAGIC = 0xdeadbeef;
+	static const unsigned short int DISK_BLOCK_SIZE = 4096;
+	static const unsigned int DISK_MAGIC = 0xdeadbeef;
 
-    Disk(const char *filename, int nblocks);
+	Disk(const char *filename, int nblocks);
 
-    int size();
-    void read(int blocknum, char * data);
-    void write(int blocknum, const char * data);
-    void close();
+	int size();
+	/* Toda vez que vocˆe
+	 *  invocar um read ou write, vocˆe deve se assegurar 
+	 *	de que data aponte para 4 KB completos de mem´oria.
+	*/
+	void read(int blocknum, char *data);
+	void write(int blocknum, const char *data);
+	void close();
 
 private:
-    void sanity_check(int blocknum, const void *data);
+	void sanity_check(int blocknum, const void *data);
 
 private:
-    FILE *diskfile;
-    int nblocks;
-    int nreads;
-    int nwrites;
+	FILE *diskfile;
+	int nblocks;
+	int nreads;
+	int nwrites;
 };
-
 
 #endif
--- fs.h
+++ fs.h
@@ -3,57 +3,76 @@
 
 #include "disk.h"
 
+/*My initial thoughts:
+* 1. The bitmap should be declared here.
+* 1.1 EVery time the disk is mounted(?), the system should build a new bitmap.
+*/
+
 class INE5412_FS
 {
 public:
-    static const unsigned int FS_MAGIC = 0xf0f03410;
-    static const unsigned short int INODES_PER_BLOCK = 128;
-    static const unsigned short int POINTERS_PER_INODE = 5;
-    static const unsigned short int POINTERS_PER_BLOCK = 1024;
+	static const unsigned int FS_MAGIC = 0xf0f03410;
+	static const unsigned short int INODES_PER_BLOCK = 128;
+	static const unsigned short int POINTERS_PER_INODE = 5;
+	static const unsigned short int POINTERS_PER_BLOCK = 1024;
 
-    class fs_superblock {
-        public:
-            unsigned int magic;
-            int nblocks;
-            int ninodeblocks;
-            int ninodes;
-    }; 
+	class fs_superblock /*A total of 16 bytes, 4 bytes each.*/
+	{
+	public:
+		/*magic = FS_MAGIC:  rotina de formatação coloca este número nos primeiros bytes do su-
+		* perbloco como um tipo de “assinatura” do sistema de arquivos.
+		* Quando o sistema de arquivos é montado, o
+		* SO procura por este número mágico. Se estiver correto, então assume-se que o disco 
+		* contém um sistema de arquivos correto. Se algum outro número estiver presente, 
+		* então a montagem falha, talvez porque o disco não esteja formatado 
+		* ou contém algum outro tipo de dado.
+		*
+		* ninodeblocks: A rotina de formatação é responsável por escolher ninodeblocks: 
+		* isto deve ser sempre 10 por cento de nblocks, arredondando pra cima.
+		*/
+		unsigned int magic;
+		int nblocks;	  /*Total number of blocks*/
+		int ninodeblocks; /*Number of blocks reserved to store inodes.*/
+		int ninodes;	  /*Number of inodes in these blocks*/
+	};
 
-    class fs_inode {
-        public:
-            int isvalid;
-            int size;
-            int direct[POINTERS_PER_INODE];
-            int indirect;
-    };
+	class fs_inode
+	{
+	public:
+		int isvalid; /*1 for valid*/
+		int size;	 /*bytes*/
+		int direct[POINTERS_PER_INODE];
+		int indirect;
+	};
 
-    union fs_block {
-        public:
-            fs_superblock super;
-            fs_inode inode[INODES_PER_BLOCK];
-            int pointers[POINTERS_PER_BLOCK];
-            char data[Disk::DISK_BLOCK_SIZE];
-    };
+	union fs_block
+	{
+	public:
+		fs_superblock super;
+		fs_inode inode[INODES_PER_BLOCK];
+		int pointers[POINTERS_PER_BLOCK];
+		char data[Disk::DISK_BLOCK_SIZE];
+	};
 
 public:
+	INE5412_FS(Disk *d)
+	{
+		disk = d;
+	}
 
-    INE5412_FS(Disk *d) {
-        disk = d;
-    } 
+	void fs_debug();
+	int fs_format();
+	int fs_mount();
 
-    void fs_debug();
-    int  fs_format();
-    int  fs_mount();
+	int fs_create();
+	int fs_delete(int inumber);
+	int fs_getsize(int inumber);
 
-    int  fs_create();
-    int  fs_delete(int inumber);
-    int  fs_getsize(int inumber);
-
-    int  fs_read(int inumber, char *data, int length, int offset);
-    int  fs_write(int inumber, const char *data, int length, int offset);
+	int fs_read(int inumber, char *data, int length, int offset);
+	int fs_write(int inumber, const char *data, int length, int offset);
 
 private:
-    Disk *disk;
+	Disk *disk;
 };
 
 #endif
